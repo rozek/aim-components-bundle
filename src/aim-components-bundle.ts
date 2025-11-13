8ff0f2fb4bd3bf0b219b227a11cb0003b7d4b97f
Andreas Rozek <A.Rozek@gmx.de>
2025-11-13 10:00:16 +0100

    Create aim-components-bundle.ts

---
+++ src/aim-components-bundle.ts
@@ -0,0 +1,18 @@
+export { html } from 'htm/preact'
+
+export {
+  createElement, cloneElement, Component,
+  createContext, createRef,
+  render, hydrate, toChildArray
+} from 'preact'
+
+export {
+  useState, useReducer, useEffect, useLayoutEffect, useRef, useImperativeHandle,
+  useMemo, useCallback, useContext, useDebugValue, useErrorBoundary, useId
+} from 'preact/hooks'
+
+export { createPortal } from 'preact/compat'
+
+export { useAutoAnimate } from '@formkit/auto-animate/preact'
+
+export * from 'aim-components'

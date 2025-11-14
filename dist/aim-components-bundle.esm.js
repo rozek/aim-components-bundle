7c399beb0d639eaa12976536ee96b03711f1a650
Andreas Rozek <A.Rozek@gmx.de>
2025-11-14 17:32:38 +0100

    Update aim-components-bundle.esm.js

--- dist/aim-components-bundle.esm.js
+++ dist/aim-components-bundle.esm.js
@@ -23280,7 +23280,18 @@ function useRerenderer() {
   return rerender;
 }
 function useConfiguration(initialConfiguration = {}) {
-  javascriptInterfaceLibrary_umdExports.allowPlainObject("initial configuration", initialConfiguration);
+  if (!javascriptInterfaceLibrary_umdExports.ValueIsFunction(initialConfiguration) && !javascriptInterfaceLibrary_umdExports.ValueIsPlainObject(initialConfiguration)) throwError(
+    "InvalidArgument:the given initial configuration is neither a plain object nor a function"
+  );
+  if (javascriptInterfaceLibrary_umdExports.ValueIsFunction(initialConfiguration)) {
+    initialConfiguration = executedCallback(
+      'component callback "initialConfiguration"',
+      initialConfiguration
+    );
+    if (!javascriptInterfaceLibrary_umdExports.ValueIsPlainObject(initialConfiguration)) throwError(
+      "InvalidArgument:the result of the initial configuration callback is not a plain object"
+    );
+  }
   const ConfigurationRef = F();
   if (ConfigurationRef.current == null) {
     ConfigurationRef.current = deepCopyOf(initialConfiguration);
